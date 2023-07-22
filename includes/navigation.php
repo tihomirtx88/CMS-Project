505ee3fef6477b3d95cea6c744694edd5b5546bc
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2023-07-22 17:44:39 +0300

    Create admin code resusable

--- includes/navigation.php
+++ includes/navigation.php
@@ -25,10 +25,10 @@
                      echo "<li><a href='#'>{$categ_title}</a></li>";
                   }
                  ?>
-                <!-- <li>
-                    <a href="#">About</a>
-                </li>
                 <li>
+                    <a href="admin">Admin</a>
+                </li>
+                <!-- <li>
                     <a href="#">Services</a>
                 </li>
                 <li>
