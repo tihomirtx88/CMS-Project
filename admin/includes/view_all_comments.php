d87afcf833c63d47f7bf7ec1af3d44e7738fda3c
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2023-07-29 13:44:56 +0300

    insert and create comment

---
+++ admin/includes/view_all_comments.php
@@ -0,0 +1,68 @@
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Author</th>
+            <th>Comment</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>In Response To</th>
+            <th>Date</th>
+            <th>Aprove</th>
+            <th>UnAprove</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        // GET ALL POSTS
+        $query = 'SELECT * FROM comments ';
+        $select_comments = mysqli_query($conection, $query);
+        while ($row = mysqli_fetch_assoc($select_comments)) {
+            $comment_id =  $row['comment_id'];
+            $comment_post_id =  $row['comment_post_id'];
+            $comment_author =  $row['comment_author'];
+            $comment_email =  $row['comment_email'];
+            $comment_status =  $row['comment_status'];
+            $comment_date =  $row['comment_date'];
+            $comment_content =  $row['comment_content'];
+        
+            echo "<tr>";
+            echo "<td>$comment_id</td>";
+ 
+            // $query = "SELECT * FROM category WHERE category_id LIKE $post_category_id ";
+            // $select_category = mysqli_query($conection, $query);
+            // while ($row = mysqli_fetch_assoc($select_category)) {
+            //     $categ_title =  $row['category_title'];
+            //     $categ_id =  $row['category_id'];
+            // echo "<td>$categ_title</td>";
+            // }
+
+            echo "<td>$comment_author</td>";
+            echo "<td>$comment_content</td>";
+            echo "<td>$comment_email</td>";
+            echo "<td>$comment_status</td>";
+            echo "<td>it resposnse for </td>";
+            echo "<td>$comment_date</td>";
+
+            
+            echo "<td><a href='posts.php?source=edit_post&p_id={$comment_id}'>Approve</a></td>";
+            echo "<td><a href='posts.php?delete={$comment_id}'>UnApprove</a></td>";
+
+            echo "<td><a href='posts.php?source=edit_post&p_id={$comment_id}'>Edit</a></td>";
+            echo "<td><a href='posts.php?delete={$comment_id}'>Delete</a></td>";
+            echo "</tr>";
+        }
+        ?>
+
+    </tbody>
+</table>
+<!-- DELETE POST  -->
+<?php
+  if (isset($_GET['delete'])) {
+    $the_post_id = $_GET['delete'];
+    $query = "DELETE FROM posts WHERE post_id LIKE {$the_post_id} ";
+    $delete_query = mysqli_query($conection, $query);
+  }
+?>
--- post.php
+++ post.php
@@ -64,15 +64,39 @@ include "includes/navigation.php";
 
 
             <!-- Blog Comments -->
+            <?php
+               if (isset($_POST['create_comment'])) {
+                 $the_post_id = $_GET['p_id'];
+                 $comment_author = $_POST['comment_author'];
+                 $comment_email = $_POST['comment_email'];
+                 $comment_content = $_POST['comment_content'];
+
+                 $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)" ;
+                 $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())" ;
+                 $create_comment_query = mysqli_query($conection, $query);
+                 if (!$create_comment_query) {
+                    die("QUERY FALIED" . mysqli_error($conection));
+                }
+               }
+            ?>
 
             <!-- Comments Form -->
             <div class="well">
                 <h4>Leave a Comment:</h4>
-                <form role="form">
+                <form role="form" action="" method="post">
                     <div class="form-group">
-                        <textarea class="form-control" rows="3"></textarea>
+                        <label for="comment_author">Author</label>
+                        <input type="text" class="form-control" name="comment_author">
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="form-group">
+                        <label for="comment_email">Email</label>
+                        <input type="email" class="form-control" name="comment_email">
+                    </div>
+                    
+                    <div class="form-group">
+                        <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                    </div>
+                    <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                 </form>
             </div>
 
