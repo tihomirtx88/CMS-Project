9a7c144075f561f1a2ab9c510afc7d064ddab23e
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2023-07-30 16:26:08 +0300

    last-changes

--- includes/login.php
+++ includes/login.php
@@ -12,8 +12,20 @@
     if (!$select_user_query) {
         die("QUERY FALIED" . mysqli_error($conection));
     }
+    
     while ($row = mysqli_fetch_array($select_user_query)) {
-       echo $db_id = $row['user_id'];
+        $user_id = $row['user_id'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_username = $row['user_username'];
+        $user_password = $row['user_password'];
+        $user_role = $row['user_role'];
+    }
+    if ($username !== $user_id && $password !== $user_password) {
+        header("Location: ../index.php");
+    }
+    else{
+        header("Location: ../admin");
     }
   }
 ?>
